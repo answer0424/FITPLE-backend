3a8ec2f175fc483c4b07e9713411c09612475a5b
Donghee Lee <dhleedev47@gmail.com>
2025-02-14 15:13:45 +0900

    Struct():sql 추가

---
+++ src/test/resources/data2.sql
@@ -0,0 +1,15 @@
+INSERT INTO user (username, password, email, nickname, address, birth, profile_image, authority, created_at)
+VALUES
+    ('User51', 'password51', 'user51@example.com', 'UserNick51', '서울특별시 종로구 수표로26길 12', '1985-05-01', '/upload/profile/profile51.png', 'ROLE_STUDENT', NOW()),
+    ('User52', 'password52', 'user52@example.com', 'UserNick52', '서울시 종로구 창신동 328-18', '1985-05-01', '/upload/profile/profile52.png', 'ROLE_STUDENT', NOW()),
+    ('User53', 'password53', 'user53@example.com', 'UserNick53', '서울시 종로구 종로3가 119번지 지하동 1층 동영타워', '1985-05-01', '/upload/profile/profile53.png', 'ROLE_STUDENT', NOW()),
+    ('User54', 'password54', 'user54@example.com', 'UserNick54', '서울특별시 종로구 수표로26길 12', '1985-05-01', '/upload/profile/profile54.png', 'ROLE_STUDENT', NOW()),
+    ('User55', 'password55', 'user55@example.com', 'UserNick55', '서울시 종로구 창신동 328-18', '1985-05-01', '/upload/profile/profile55.png', 'ROLE_STUDENT', NOW()),
+    ('User56', 'password56', 'user56@example.com', 'UserNick56', '서울시 종로구 종로3가 119번지 지하동 1층 동영타워', '1985-05-01', '/upload/profile/profile56.png', 'ROLE_STUDENT', NOW()),
+    ('User57', 'password57', 'user57@example.com', 'UserNick57', '서울특별시 종로구 수표로26길 12', '1985-05-01', '/upload/profile/profile57.png', 'ROLE_STUDENT', NOW()),
+    ('User58', 'password58', 'user58@example.com', 'UserNick58', '서울시 종로구 창신동 328-18', '1985-05-01', '/upload/profile/profile58.png', 'ROLE_STUDENT', NOW()),
+    ('User59', 'password59', 'user59@example.com', 'UserNick59', '서울시 종로구 종로3가 119번지 지하동 1층 동영타워', '1985-05-01', '/upload/profile/profile59.png', 'ROLE_STUDENT', NOW()),
+    ('User60', 'password60', 'user60@example.com', 'UserNick60', '서울특별시 종로구 수표로26길 12', '1985-05-01', '/upload/profile/profile60.png', 'ROLE_STUDENT', NOW()),
+    ('User61', 'password61', 'user61@example.com', 'UserNick61', '서울시 종로구 창신동 328-18', '1985-05-01', '/upload/profile/profile61.png', 'ROLE_STUDENT', NOW()),
+    ('User62', 'password62', 'user62@example.com', 'UserNick62', '서울시 종로구 종로3가 119번지 지하동 1층 동영타워', '1985-05-01', '/upload/profile/profile62.png', 'ROLE_STUDENT', NOW())
+;
