48cd0700387cbceafa4da48c246fb65460691cce
answer0424 <wonwon123123@naver.com>
2025-02-17 08:42:16 +0900

    fix(#1): 수정 5

--- src/main/java/com/lec/spring/base/service/UserService.java
+++ src/main/java/com/lec/spring/base/service/UserService.java
@@ -129,7 +129,9 @@ public class UserService {
                 .orElseThrow(() -> new EntityNotFoundException("유저 검색에 실패했습니다."));
         try {
             String ddir = imgService.saveImage(image, dir);
+            System.out.println("UserService에서 저장되는 경로래 : " + ddir);
             user.setProfileImage(ddir);
+            System.out.println("UserService에서 저장된느 파일명이래 : " + user.getProfileImage());
             userRepository.saveAndFlush(user);
             return true;
         } catch (IOException e) {
